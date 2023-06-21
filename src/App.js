3e03b179fcb64fc6e388aaf4db55ae44cafbc628
Prakarsh Gupta <prakarshgupta02051@gmail.com>
2023-06-21 17:14:34 +0530

    Checking paraphrase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Header } from './Components/Header';
 import { Main } from './Components/Main';
 import { Footer } from './Components/Footer';
-// ptechofficial.github.io
 
 function App() {
   return (
